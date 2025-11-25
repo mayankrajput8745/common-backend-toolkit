bb55e200548e094f9143d1eb8848c0a479cdb513
Mayank Rajput <mayankrajput9354@gmail.com>
2025-11-25 12:27:23 +0530

    Refactor JWT helper methods to remove promisify and streamline error handling

--- src/helpers/jwt.ts
+++ src/helpers/jwt.ts
@@ -1,52 +1,36 @@
-import { promisify } from "util";
 import { verify, sign, decode, SignOptions } from "jsonwebtoken";
-
 
 export class JWT {
 
-    async encode(payload: JwtPayload, privateKey: string, options: SignOptions): Promise<string> {
+    async encode(payload: JwtPayload, privateKey: string, options: SignOptions = {}): Promise<string> {
         if (!privateKey) throw new Error("Private key is required");
 
-        // @ts-ignore
-        return promisify(sign)({ ...payload }, privateKey, options);
+        return new Promise((resolve, reject) => {
+            sign({ ...payload }, privateKey, options, (err, token) => {
+                if (err || !token) return reject(err);
+                resolve(token);
+            });
+        });
     }
 
-    async decode(token: string, publicKey: string, validation: ValidationsParams): Promise<JwtPayload> {
-        try {
-            if (!publicKey) throw new Error("Public key is required");
-
-            // @ts-ignore
-            return <JwtPayload>promisify(decode)(token, publicKey, validation);
-        } catch (error: any) {
-
-            if (error && error.name === 'TokenExpiredError') {
-
-                // @ts-ignore
-                return <JwtPayload>promisify(decode)(token, publicKey, validation);
-            } else {
-                throw error;
-            }
-        }
+    async decode(token: string): Promise<JwtPayload | null> {
+        return decode(token, { json: true }) as JwtPayload | null;
     }
-
     async verify(token: string, publicKey: string, validation: ValidationsParams): Promise<JwtPayload> {
-        try {
-            if (!publicKey) throw new Error("Public key is required");
+        if (!publicKey) throw new Error("Public key is required");
 
-            // @ts-ignore
-            return <JwtPayload>promisify(verify)(token, publicKey, validation);
-        } catch (error: any) {
-            if (error && error.name === 'TokenExpiredError') {
-
-                // @ts-ignore
-                throw error;
-            } else {
-                throw error;
-            }
-        }
+        return new Promise((resolve, reject) => {
+            verify(
+                token,
+                publicKey,
+                validation,
+                (err, decoded) => {
+                    if (err) return reject(err);
+                    resolve(decoded as JwtPayload);
+                }
+            );
+        });
     }
-
-
 }
 
 export class ValidationsParams {
@@ -66,32 +50,21 @@ type Payload = {
     iss: string;
     aud: string;
     userId: string;
-    iat: number;
-    exp: number;
-    validity: number;
     data: any;
     tokenId: string;
 }
-
-const HOUR_IN_DAY = 24;
-const MINUTES_IN_HOUR = 60;
-const SECONDS_IN_MINUTE = 60;
 
 export class JwtPayload {
     iss: string;
     aud: string;
     userId: string;
-    iat: number;
-    exp: number;
     data: any;
     tokenId: string;
 
-    constructor({ iss, aud, userId, validity, data, tokenId }: Payload) {
+    constructor({ iss, aud, userId, data, tokenId }: Payload) {
         this.iss = iss;
         this.aud = aud;
         this.userId = userId;
-        this.iat = Math.floor(Date.now() / 1000);
-        this.exp = this.iat + (validity / HOUR_IN_DAY * MINUTES_IN_HOUR * SECONDS_IN_MINUTE);
         this.data = data;
         this.tokenId = tokenId;
     }
